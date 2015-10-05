2abc8d52c33ce342c813985bc7cff5b84af72bdd
relvaner <relvaner@googlemail.com>
2015-10-05 19:10:23 +0200

    Update

--- actor4j-m-benchmark/src/test/java/Sizeof.java
+++ actor4j-m-benchmark/src/test/java/Sizeof.java
@@ -1,16 +1,16 @@
 import actor4j.core.Actor;
 import actor4j.core.ActorMessage;
 
-public class Sizeof
+public class Sizeof 
 {
-    public static void main (String [] args) throws Exception
+    public static void main(String[] args) throws Exception 
     {
         // Warm up all classes/methods we will use
-        runGC ();
-        usedMemory ();
+        runGC();
+        usedMemory();
         // Array to keep strong references to allocated objects
         final int count = 100000;
-        Object [] objects = new Object [count];
+        Object[] objects = new Object[count];
         
         long heap1 = 0;
         // Allocate count+1 objects, discard the first one
@@ -29,44 +29,44 @@ public class Sizeof
             else
             {
                 object = null; // Discard the warm up object
-                runGC ();
-                heap1 = usedMemory (); // Take a before heap snapshot
+                runGC();
+                heap1 = usedMemory(); // Take a before heap snapshot
             }
         }
         runGC ();
-        long heap2 = usedMemory (); // Take an after heap snapshot:
+        long heap2 = usedMemory(); // Take an after heap snapshot:
         
-        final int size = Math.round (((float)(heap2 - heap1))/count);
+        final int size = Math.round(((float)(heap2 - heap1))/count);
         System.out.println ("'before' heap: " + heap1 +
                             ", 'after' heap: " + heap2);
         System.out.println ("heap delta: " + (heap2 - heap1) +
-            ", {" + objects [0].getClass () + "} size = " + size + " bytes");
+            ", {" + objects [0].getClass() + "} size = " + size + " bytes");
         for (int i = 0; i < count; ++ i) objects [i] = null;
         objects = null;
     }
-    private static void runGC () throws Exception
+    private static void runGC() throws Exception
     {
         // It helps to call Runtime.gc()
         // using several method calls:
-        for (int r = 0; r < 4; ++ r) _runGC ();
+        for (int r = 0; r < 4; ++ r) _runGC();
     }
-    private static void _runGC () throws Exception
+    private static void _runGC() throws Exception
     {
-        long usedMem1 = usedMemory (), usedMem2 = Long.MAX_VALUE;
+        long usedMem1 = usedMemory(), usedMem2 = Long.MAX_VALUE;
         for (int i = 0; (usedMem1 < usedMem2) && (i < 500); ++ i)
         {
-            s_runtime.runFinalization ();
-            s_runtime.gc ();
-            Thread.currentThread ().yield ();
+            s_runtime.runFinalization();
+            s_runtime.gc();
+            Thread.yield();
             
             usedMem2 = usedMem1;
-            usedMem1 = usedMemory ();
+            usedMem1 = usedMemory();
         }
     }
-    private static long usedMemory ()
+    private static long usedMemory()
     {
-        return s_runtime.totalMemory () - s_runtime.freeMemory ();
+        return s_runtime.totalMemory() - s_runtime.freeMemory();
     }
     
-    private static final Runtime s_runtime = Runtime.getRuntime ();
+    private static final Runtime s_runtime = Runtime.getRuntime();
 } // End of class
