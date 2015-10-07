9d12aa49cd20edd34c7c033c60c7d5c5a873a771
relvaner <relvaner@googlemail.com>
2015-10-07 22:51:55 +0200

    Added logger

---
+++ actor4j-m-core/src/main/java/actor4j/core/ActorLogger.java
@@ -0,0 +1,26 @@
+package actor4j.core;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+public class ActorLogger {
+	static ActorLogger actorLogger;
+	
+	protected Logger logger;
+	
+	protected final String layout_console = "%d{yyyy-MM-dd hh:mm:ss,SSS}\t%-5p %m%n";
+	
+	static {
+		actorLogger = new ActorLogger();
+	}
+	
+	private ActorLogger() {
+		logger = Logger.getLogger(this.getClass());
+		logger.addAppender(new ConsoleAppender(new PatternLayout(layout_console)));
+	}
+	
+	public static Logger logger() {
+		return actorLogger.logger;
+	}
+}
