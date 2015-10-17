1436d08545f538a096c289510eb83ad86c5f4213
relvaner <relvaner@googlemail.com>
2015-10-17 16:08:49 +0200

    Improvement

--- actor4j-m-core/src/main/java/actor4j/core/balancing/ActorBalancingOnCreationManual.java
+++ actor4j-m-core/src/main/java/actor4j/core/balancing/ActorBalancingOnCreationManual.java
@@ -12,7 +12,7 @@ import actor4j.core.ActorThread;
 import actor4j.core.utils.ActorGroup;
 
 public abstract class ActorBalancingOnCreationManual {
-	public abstract void balance(Map<UUID, Long> actorsMap, List<ActorThread> actorThreads);
+	public abstract void balance(Map<UUID, Long> actorsMap, List<ActorThread> actorThreads, Map<UUID, Long> groupsMap);
 	
 	public void balanceActor(Map<UUID, Long> actorsMap, Long threadId, Actor actor) {
 		actorsMap.put(actor.getId(), threadId);
@@ -28,15 +28,21 @@ public abstract class ActorBalancingOnCreationManual {
 		}
 	}
 	
-	public void balanceGroup(Map<UUID, Long> actorsMap, Long threadId, ActorGroup group) {
+	public void balanceGroup(Map<UUID, Long> actorsMap, Map<UUID, Long> groupsMap, Long threadId, ActorGroup group) {
+		Long foundThreadId = groupsMap.get(group.getId());
+		if (foundThreadId==null)
+			groupsMap.put(group.getId(), threadId);
+		else
+			threadId = foundThreadId;
+		
 		for (UUID id : group)
 			actorsMap.put(id, threadId);
 	}
 	
-	public void balanceGroups(Map<UUID, Long> actorsMap, List<ActorThread> actorThreads, List<ActorGroup> groups) {
+	public void balanceGroups(Map<UUID, Long> actorsMap, List<ActorThread> actorThreads, Map<UUID, Long> groupsMap, List<ActorGroup> groups) {
 		int i=0;
 		for (ActorGroup group : groups) {
-			balanceGroup(actorsMap, actorThreads.get(i).getId(), group);
+			balanceGroup(actorsMap, groupsMap, actorThreads.get(i).getId(), group);
 			i++;
 			if (i==actorThreads.size())
 				i = 0;
