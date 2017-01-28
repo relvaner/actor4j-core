ef9be6022d4785759cab90d88e3dba8c0b4798d9
relvaner <relvaner@gmail.com>
2017-01-28 22:30:38 +0100

    Update

---
+++ actor4j-core/src/main/java/actor4j/core/reactive/streams/ProcessorActor.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2015-2017, David A. Bauer
+ */
+package actor4j.core.reactive.streams;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import actor4j.core.actors.Actor;
+import actor4j.core.messages.ActorMessage;
+import tools4j.function.Procedure;
+
+public class ProcessorActor extends Actor {
+	protected ProcessorImpl processorImpl;
+	
+	public ProcessorActor() {
+		this(null);
+	}
+	
+	public ProcessorActor(String name) {
+		super(name);
+		processorImpl = new ProcessorImpl(this);
+	}
+	
+	@Override
+	public void receive(ActorMessage<?> message) {
+		processorImpl.receive(message);
+	}
+	
+	public <T> void broadcast(T value) {
+		processorImpl.broadcast(value);
+	}
+	
+	public boolean isBulk(UUID dest) {
+		return processorImpl.isBulk(dest);
+	}
+	
+	public <T> boolean onNext(T value, UUID dest) {
+		return processorImpl.onNext(value, dest);
+	}
+	
+	public void onError(String error, UUID dest) {
+		processorImpl.onError(error, dest);
+	}
+	
+	public void onComplete(UUID dest) {
+		processorImpl.onComplete(dest);
+	}
+	
+	public void subscribe(UUID dest, Consumer<Object> onNext, Consumer<String> onError, Procedure onComplete) {
+		processorImpl.subscribe(dest, onNext, onError, onComplete);
+	}
+	
+	public void unsubscribe(UUID dest) {
+		processorImpl.unsubscribe(dest);
+	}
+	
+	public void request(long n, UUID dest) {
+		processorImpl.request(n, dest);
+	}
+	
+	public void requestReset(long n, UUID dest) {
+		processorImpl.requestReset(n, dest);
+	}
+	
+	public void bulk(UUID dest) {
+		processorImpl.bulk(dest);
+	}
+	
+	public void cancelBulk(UUID dest) {
+		processorImpl.cancelBulk(dest);
+	}
+}
