c880a5ce749bdb46c080dc18d43fbf65bab80e1e
relvaner <relvaner@gmail.com>
2017-01-30 19:59:09 +0100

    Update

--- actor4j-core/src/test/java/actor4j/core/features/UnhandledFeature.java
+++ actor4j-core/src/test/java/actor4j/core/features/UnhandledFeature.java
@@ -1,29 +1,59 @@
+/*
+ * Copyright (c) 2015-2017, David A. Bauer
+ */
 package actor4j.core.features;
 
 import java.util.UUID;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import actor4j.core.ActorSystem;
 import actor4j.core.actors.Actor;
 import actor4j.core.messages.ActorMessage;
 import actor4j.core.utils.ActorFactory;
 
+import static actor4j.core.utils.ActorLogger.logger;
+
 public class UnhandledFeature {
 	protected ActorSystem system;
 
+	@Before
 	public void before() {
 		system = new ActorSystem();
 		system.setParallelismMin(1);
 		system.setDebugUnhandled(true);
 	}
 	
+	@Test
 	public void test() {
 		UUID dest = system.addActor(new ActorFactory() { 
 			@Override
 			public Actor create() {
 				return new Actor("UnhandledFeatureActor") {
+					@Mock
+					protected Appender mockAppender;
+					@Captor
+					protected ArgumentCaptor<LoggingEvent> captorLoggingEvent;
+					
 					@Override
 					public void receive(ActorMessage<?> message) {
+						MockitoAnnotations.initMocks(this);
+						logger().removeAllAppenders();
+						logger().addAppender(mockAppender);
 						unhandled(message);
+						verify(mockAppender, times(1)).doAppend(captorLoggingEvent.capture());
+						LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+						assertTrue(loggingEvent.getMessage().toString().contains("Unhandled message"));
 					}
 				};
 			}
