79f13767836ec9a0631e1c829314746b9cbc9fd9
relvaner <relvaner@gmail.com>
2017-02-16 22:57:20 +0100

    Update

--- actor4j-core/src/main/java/actor4j/core/data/DataAccessObject.java
+++ actor4j-core/src/main/java/actor4j/core/data/DataAccessObject.java
@@ -26,4 +26,10 @@ public class DataAccessObject<K, V> {
 		this.collectionName = collectionName;
 		this.source = source;
 	}
+
+	@Override
+	public String toString() {
+		return "DataAccessObject [key=" + key + ", value=" + value + ", filter=" + filter + ", update=" + update
+				+ ", collectionName=" + collectionName + ", source=" + source + ", reserved=" + reserved + "]";
+	}
 }
