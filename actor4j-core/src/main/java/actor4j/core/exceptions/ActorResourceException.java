7cc5d61782cab3b54a87a4f21dfc4bdcfe9bef8b
relvaner <relvaner@gmail.com>
2017-06-27 01:30:02 +0200

    Added License

--- actor4j-core/src/main/java/actor4j/core/exceptions/ActorResourceException.java
+++ actor4j-core/src/main/java/actor4j/core/exceptions/ActorResourceException.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015-2017, David A. Bauer. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 package actor4j.core.exceptions;
 
 public class ActorResourceException extends RuntimeException {
--- actor4j-core/src/main/java/actor4j/core/pattern/actors/FutureActor.java
+++ actor4j-core/src/main/java/actor4j/core/pattern/actors/FutureActor.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2015-2017, David A. Bauer
+ * Copyright (c) 2015-2017, David A. Bauer. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
  */
 package actor4j.core.pattern.actors;
 
