8da129eeb41d6106c594a1e4e18fac885edb695d
relvaner <david4bauer@googlemail.com>
2020-01-30 20:49:46 +0100

    Updated ActorLogger

--- src/main/java/io/actor4j/core/utils/ActorLogger.java
+++ src/main/java/io/actor4j/core/utils/ActorLogger.java
@@ -15,24 +15,25 @@
  */
 package io.actor4j.core.utils;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.config.AppenderRef;
-import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.apache.logging.log4j.core.config.builder.api.AppenderComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 
 public class ActorLogger {
 	static ActorLogger actorLogger;
 	
-	protected Logger logger;
-	
-	public static final String LOGGER_NAME = "actor4j-logger";
-	public static final String CONSOLE_APPENDER_NAME = "actor4j-console-appender";
-	protected final String LAYOUT_CONSOLE = "%d{yyyy-MM-dd hh:mm:ss,SSS}\t%-5p %m%n";
+	protected final LoggerContext loggerContext;
+	protected final LoggerConfig loggerConfig;
+	protected final String LAYOUT_PATTERN_CONSOLE = "%d{yyyy-MM-dd hh:mm:ss,SSS}\t%-5p %m%n";
 	
 	static {
 		actorLogger = new ActorLogger();
@@ -40,27 +41,30 @@ public class ActorLogger {
 	
 	// @See: https://logging.apache.org/log4j/2.x/manual/customconfig.html
 	private ActorLogger() {
-		LoggerContext loggerContext = (LoggerContext) LogManager.getContext();
-		Configuration config = loggerContext.getConfiguration();
-
-		Appender consoleAppender = ConsoleAppender.newBuilder()
-				.setName(CONSOLE_APPENDER_NAME)
-				.setLayout(PatternLayout.newBuilder().withPattern(LAYOUT_CONSOLE).build())
-				.setConfiguration(loggerContext.getConfiguration())
-				.build();
-		consoleAppender.start();
-	    AppenderRef[] appenderRefs = new AppenderRef[]{AppenderRef.createAppenderRef(CONSOLE_APPENDER_NAME, null, null)};
-	    LoggerConfig loggerConfig = LoggerConfig.createLogger(false, null, LOGGER_NAME, "true", appenderRefs, null, config, null);
-	    loggerConfig.addAppender(consoleAppender, null, null);
+		ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+		builder.setStatusLevel(Level.ERROR);
+		builder.setConfigurationName("actor4j");
+		AppenderComponentBuilder appenderBuilder = builder.newAppender("Stdout", "CONSOLE")
+			.addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
+		appenderBuilder.add(builder.newLayout("PatternLayout")
+			.addAttribute("pattern", LAYOUT_PATTERN_CONSOLE));
+		appenderBuilder.add(builder.newFilter("MarkerFilter", Filter.Result.DENY, Filter.Result.NEUTRAL)
+			.addAttribute("marker", "FLOW"));
+		builder.add(appenderBuilder);
+		builder.add(builder.newLogger("org.apache.logging.log4j", Level.DEBUG)
+			.add(builder.newAppenderRef("Stdout")).addAttribute("additivity", false));
+		builder.add(builder.newRootLogger(Level.DEBUG).add(builder.newAppenderRef("Stdout")));
+		loggerContext = Configurator.initialize(builder.build());
 		
-	    config.addAppender(consoleAppender);
-	    config.addLogger(LOGGER_NAME, loggerConfig);
-		loggerContext.updateLoggers(config);
-		
-		logger = LogManager.getContext().getLogger(LOGGER_NAME); 
+		loggerConfig = loggerContext.getConfiguration().getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
 	}
 	
 	public static Logger logger() {
-		return actorLogger.logger;
+		return actorLogger.loggerContext.getRootLogger();
+	}
+	
+	public static void setLevel(Level level) { 
+		actorLogger.loggerConfig.setLevel(level);
+		actorLogger.loggerContext.updateLoggers();
 	}
 }
