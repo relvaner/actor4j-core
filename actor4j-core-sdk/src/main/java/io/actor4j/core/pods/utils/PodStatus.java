31008db6a7f37fff9037d519bda62da5e32284e2
relvaner <david4bauer@googlemail.com>
2022-09-20 20:05:50 +0200

    Update

--- actor4j-core-sdk/src/main/java/io/actor4j/core/pods/utils/PodStatus.java
+++ actor4j-core-sdk/src/main/java/io/actor4j/core/pods/utils/PodStatus.java
@@ -21,23 +21,23 @@ import java.util.Map;
 public class PodStatus {
 	protected static final Map<Integer, String> statusMap;
 	
-	public static final int OK = 200; 
-	public static final int CREATED = 201; 
-	public static final int ACCEPTED = 202; 
+	public static final int OK 						= 200; 
+	public static final int CREATED 				= 201; 
+	public static final int ACCEPTED 				= 202; 
 	
-	public static final int BAD_REQUEST = 400;
-	public static final int UNAUTHORIZED = 401;
-	public static final int FORBIDDED = 403; 
-	public static final int NOT_FOUND = 404; 
-	public static final int METHOD_NOT_ALLOWED = 405;
-	public static final int NOT_ACCEPTABALE = 406;
+	public static final int BAD_REQUEST 			= 400;
+	public static final int UNAUTHORIZED 			= 401;
+	public static final int FORBIDDED 				= 403; 
+	public static final int NOT_FOUND 				= 404; 
+	public static final int METHOD_NOT_ALLOWED 		= 405;
+	public static final int NOT_ACCEPTABALE 		= 406;
 	public static final int AUTHENTICATION_REQUIRED = 407;
-	public static final int CONFLICT = 409;
-	public static final int GONE = 410;
+	public static final int CONFLICT 				= 409;
+	public static final int GONE 					= 410;
 	
-	public static final int INTERNAL_SERVER_ERROR = 500;
-	public static final int NOT_IMPLEMENTED = 501;
-	public static final int SERVICE_UNAVAILABLE = 503;
+	public static final int INTERNAL_SERVER_ERROR 	= 500;
+	public static final int NOT_IMPLEMENTED 		= 501;
+	public static final int SERVICE_UNAVAILABLE 	= 503;
 	
 	static {
 		statusMap = new HashMap<>();
