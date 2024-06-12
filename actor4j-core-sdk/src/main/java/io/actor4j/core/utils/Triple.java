7bbe1a56ae3822a19e98a349fe666b0e237675f4
relvaner <david4bauer@googlemail.com>
2024-06-12 16:17:13 +0200

    -sdk: Added class Triple

---
+++ actor4j-core-sdk/src/main/java/io/actor4j/core/utils/Triple.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015-2024, David A. Bauer. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.actor4j.core.utils;
+
+public record Triple<A, B, C>(A a, B b, C c) {
+	public static <A, B, C> Triple<A, B, C> of(A a, B b, C c) {
+		return new Triple<A, B, C>(a, b, c);
+	}
+}
