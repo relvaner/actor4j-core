cae6859e96295ce71d9a70bb3824a6497566b6cb
relvaner <david4bauer@googlemail.com>
2024-07-23 21:30:45 +0200

    -sdk: Update PodRequestMethod

--- actor4j-core-sdk/src/main/java/io/actor4j/core/pods/utils/PodRequestMethod.java
+++ actor4j-core-sdk/src/main/java/io/actor4j/core/pods/utils/PodRequestMethod.java
@@ -47,4 +47,70 @@ public final class PodRequestMethod {
 	public static final int ACTION_18   = 28;
 	public static final int ACTION_19   = 29;
 	public static final int ACTION_20   = 30;
+	
+	public static final int COMMAND_1   = 101;
+	public static final int COMMAND_2   = 102;
+	public static final int COMMAND_3   = 103;
+	public static final int COMMAND_4   = 104;
+	public static final int COMMAND_5   = 105;
+	public static final int COMMAND_6   = 106;
+	public static final int COMMAND_7   = 107;
+	public static final int COMMAND_8   = 108;
+	public static final int COMMAND_9   = 109;
+	public static final int COMMAND_10  = 110;
+	
+	public static final int COMMAND_11  = 111;
+	public static final int COMMAND_12  = 112;
+	public static final int COMMAND_13  = 113;
+	public static final int COMMAND_14  = 114;
+	public static final int COMMAND_15  = 115;
+	public static final int COMMAND_16  = 116;
+	public static final int COMMAND_17  = 117;
+	public static final int COMMAND_18  = 118;
+	public static final int COMMAND_19  = 119;
+	public static final int COMMAND_20  = 120;
+	
+	public static final int QUERY_1     = 201;
+	public static final int QUERY_2     = 202;
+	public static final int QUERY_3     = 203;
+	public static final int QUERY_4     = 204;
+	public static final int QUERY_5     = 205;
+	public static final int QUERY_6     = 206;
+	public static final int QUERY_7     = 207;
+	public static final int QUERY_8     = 208;
+	public static final int QUERY_9     = 209;
+	public static final int QUERY_10    = 210;
+	
+	public static final int QUERY_11    = 211;
+	public static final int QUERY_12    = 212;
+	public static final int QUERY_13    = 213;
+	public static final int QUERY_14    = 214;
+	public static final int QUERY_15    = 215;
+	public static final int QUERY_16    = 216;
+	public static final int QUERY_17    = 217;
+	public static final int QUERY_18    = 218;
+	public static final int QUERY_19    = 219;
+	public static final int QUERY_20    = 220;
+	
+	public static final int EVENT_1     = 301;
+	public static final int EVENT_2     = 302;
+	public static final int EVENT_3     = 303;
+	public static final int EVENT_4     = 304;
+	public static final int EVENT_5     = 305;
+	public static final int EVENT_6     = 306;
+	public static final int EVENT_7     = 307;
+	public static final int EVENT_8     = 308;
+	public static final int EVENT_9     = 309;
+	public static final int EVENT_10    = 310;
+	
+	public static final int EVENT_11    = 311;
+	public static final int EVENT_12    = 312;
+	public static final int EVENT_13    = 313;
+	public static final int EVENT_14    = 314;
+	public static final int EVENT_15    = 315;
+	public static final int EVENT_16    = 316;
+	public static final int EVENT_17    = 317;
+	public static final int EVENT_18    = 318;
+	public static final int EVENT_19    = 319;
+	public static final int EVENT_20    = 320;
 }
