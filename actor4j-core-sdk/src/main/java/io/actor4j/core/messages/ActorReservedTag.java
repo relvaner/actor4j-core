aa719d4462c44d4baa6d219926097d0038039e6d
relvaner <david4bauer@googlemail.com>
2024-07-27 21:04:18 +0200

    -sdk: Update for ActorReservedTag

--- actor4j-core-sdk/src/main/java/io/actor4j/core/messages/ActorReservedTag.java
+++ actor4j-core-sdk/src/main/java/io/actor4j/core/messages/ActorReservedTag.java
@@ -21,34 +21,43 @@ import static io.actor4j.core.logging.ActorLogger.systemLogger;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ActorReservedTag {
-	public static final Set<Integer> actorTags = ConcurrentHashMap.newKeySet();
+public final class ActorReservedTag {
+	private static final Set<Integer> actorTags = ConcurrentHashMap.newKeySet();
 
-	public static final int RESERVED_CACHE_EVICT   = checkTag(Integer.MAX_VALUE-200);
-	public static final int RESERVED_CACHE_GET     = checkTag(Integer.MAX_VALUE-201);
-	public static final int RESERVED_CACHE_SET     = checkTag(Integer.MAX_VALUE-202);
-	public static final int RESERVED_CACHE_UPDATE  = checkTag(Integer.MAX_VALUE-203);
-	public static final int RESERVED_CACHE_DEL     = checkTag(Integer.MAX_VALUE-204);
-	public static final int RESERVED_CACHE_DEL_ALL = checkTag(Integer.MAX_VALUE-205);
-	public static final int RESERVED_CACHE_CLEAR   = checkTag(Integer.MAX_VALUE-206);
-	public static final int RESERVED_CACHE_CAS     = checkTag(Integer.MAX_VALUE-207); // CompareAndSet
-	public static final int RESERVED_CACHE_CAU     = checkTag(Integer.MAX_VALUE-208); // CompareAndUpdate
+	public static final int RESERVED_CACHE_EVICT   = reservedTag(Integer.MAX_VALUE-200);
+	public static final int RESERVED_CACHE_GET     = reservedTag(Integer.MAX_VALUE-201);
+	public static final int RESERVED_CACHE_SET     = reservedTag(Integer.MAX_VALUE-202);
+	public static final int RESERVED_CACHE_UPDATE  = reservedTag(Integer.MAX_VALUE-203);
+	public static final int RESERVED_CACHE_DEL     = reservedTag(Integer.MAX_VALUE-204);
+	public static final int RESERVED_CACHE_DEL_ALL = reservedTag(Integer.MAX_VALUE-205);
+	public static final int RESERVED_CACHE_CLEAR   = reservedTag(Integer.MAX_VALUE-206);
+	public static final int RESERVED_CACHE_CAS     = reservedTag(Integer.MAX_VALUE-207); // CompareAndSet
+	public static final int RESERVED_CACHE_CAU     = reservedTag(Integer.MAX_VALUE-208); // CompareAndUpdate
 	public static final int RESERVED_CACHE_SUBSCRIBE_SECONDARY = checkTag(Integer.MAX_VALUE-209);
 	
-	public static final int RESERVED_PUBLISH_SERVICE   = checkTag(Integer.MAX_VALUE-100);
-	public static final int RESERVED_UNPUBLISH_SERVICE = checkTag(Integer.MAX_VALUE-101);
-	public static final int RESERVED_LOOKUP_SERVICES   = checkTag(Integer.MAX_VALUE-102);
-	public static final int RESERVED_LOOKUP_SERVICE    = checkTag(Integer.MAX_VALUE-103);
+	public static final int RESERVED_PUBLISH_SERVICE   = reservedTag(Integer.MAX_VALUE-100);
+	public static final int RESERVED_UNPUBLISH_SERVICE = reservedTag(Integer.MAX_VALUE-101);
+	public static final int RESERVED_LOOKUP_SERVICES   = reservedTag(Integer.MAX_VALUE-102);
+	public static final int RESERVED_LOOKUP_SERVICE    = reservedTag(Integer.MAX_VALUE-103);
 	
-	public static final int RESERVED_UNHANDLED = checkTag(Integer.MAX_VALUE-2);
+	public static final int RESERVED_UNHANDLED = reservedTag(Integer.MAX_VALUE-2);
 	
-	public static final int RESERVED_UP    	   = checkTag(Integer.MAX_VALUE-1); // HEALTH_CHECK_SUCCESS
-	public static final int RESERVED_TIMEOUT   = checkTag(Integer.MAX_VALUE);
+	public static final int RESERVED_UP    	   = reservedTag(Integer.MAX_VALUE-1); // HEALTH_CHECK_SUCCESS
+	public static final int RESERVED_TIMEOUT   = reservedTag(Integer.MAX_VALUE);
 	
 	static {
 		// PodStatus
 		for (int i=200; i<600; i++)
 			actorTags.add(i);
+	}
+	
+	private static int reservedTag(int tag) {
+		if (tag<0)
+			systemLogger().log(ERROR, String.format("[FATAL] Reserved tags below zero are system tags: %s", tag));
+		else if (!actorTags.add(tag))
+			systemLogger().log(ERROR, String.format("Reserved tag already exists: %s", tag));
+		
+		return tag;
 	}
 	
 	public static int checkTag(int tag) {
